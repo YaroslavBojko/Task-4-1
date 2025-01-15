8a411aa5de1350f3a308f654121731519a2148c2
Ярослав <144372173+YaroslavBojko@users.noreply.github.com>
2025-01-15 17:49:27 +0300

    Entering numbers

--- main.cpp
+++ main.cpp
@@ -6,4 +6,15 @@ int main()
 {
 	setlocale(LC_ALL, "Rus");
 
+	int firstNumber;
+	cout << "Введите первое число: ";
+	cin >> firstNumber;
+
+	int secondNumber;
+	cout << "Введите второе число: ";
+	cin >> secondNumber;
+
+	cout << "-----Проверяем-----" << endl;
+
+	cout << "Наименьшее число: ";
 }
